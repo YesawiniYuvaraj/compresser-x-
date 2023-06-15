f1398484ea31c721fb28fda3887dc3cf22d2ca5f
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2023-06-15 21:16:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,17 +16,17 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1322549723
-    OWNER = config("OWNER")
+    APP_ID = config("APP_ID", default=18990697, cast=int)
+    API_HASH = config("API_HASH", default="f4815b9a16cb03c2f5eabe8db1cb0903")
+    BOT_TOKEN = config("5603895273:AAH_OWCXHDvEk4pIVZFqqDTIMnqJ37HP5O0")
+    DEV = 5261482689
+    OWNER = config("5261482689")
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/75ee20ec8d8c8bba84f02.jpg"
+        "THUMBNAIL", default="https://graph.org/file/bdc936caa8040f7645c0c.jpg"
     )
 except Exception as e:
     print("Environment vars Missing")
