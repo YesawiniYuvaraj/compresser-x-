9c4008ed97447a36f008f23c2553e90d7771ec28
Sanjeev <116713311+YesawiniYuvaraj@users.noreply.github.com>
2024-11-08 22:01:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,21 +1,3 @@
-#    This file is part of the Compressor distribution.
-#    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
-
-import config
-
-try:
     APP_ID = config("APP_ID", default=18990697, cast=int)
     API_HASH = config("API_HASH", default="f4815b9a16cb03c2f5eabe8db1cb0903")
     BOT_TOKEN = config("6574755955:AAFr3tBM_bnzpBVWTNFysU1MV5RyKGe_kck")
